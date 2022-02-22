8ca1173491d08e54712cd3fee8f6a34a27efefa8
Luis Gonzalez <luisgonzalez@Luiss-MacBook-Pro.local>
2022-02-21 19:22:12 -0500

    Movies React

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+// import Movie from './Movie';
 
 export default function MovieList(props) {
   return (
@@ -15,6 +17,8 @@ function MovieDetails(props) {
 
   return (
     <div className="movie-card">
+      <Link to={`/movies/${props.movie.id}`}>
+
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -22,6 +26,7 @@ function MovieDetails(props) {
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
+      </Link>
     </div>
   );
 }
